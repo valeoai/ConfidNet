4f1e1f9e0f5776a9aa19815864599b91396c0038
gabrieldemarmiesse <gabrieldemarmiesse@gmail.com>
2019-09-25 17:04:45 +0000

    Trying to move the compilation of the cpp at install time;

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM nvidia/cuda:10.0-base as base_image
+FROM nvidia/cuda:10.0-runtime as base_image
 
 RUN apt-get update && apt-get install -y wget bzip2
 RUN wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
@@ -8,8 +8,9 @@ ENV PATH="/opt/conda/bin:${PATH}"
 RUN conda config --set always_yes yes
 
 RUN conda install pytorch torchvision cudatoolkit=10.0 -c pytorch
-RUN conda install -c menpo opencv
-RUN pip install tensorflow
+RUN pip install tensorflow torchsummary pyyaml verboselogs coloredlogs
+
+RUN apt-get update && apt-get install -y gcc g++
 
 COPY ./ ./ConfidNet
 RUN pip install -e ./ConfidNet
@@ -17,3 +18,4 @@ RUN pip install -e ./ConfidNet
 WORKDIR ./ConfidNet
 
 RUN python -c "import confidnet"
+RUN python -c "import confidnet.structured_map_ranking_loss"
--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 from setuptools import find_packages
 from setuptools import setup
+from torch.utils.cpp_extension import CppExtension, BuildExtension
 
-setup(name='ConfidNet',
-      author='Charles Corbiere',
-      url='https://github.com/valeoai/ConfidNet',
-      install_requires=['pyyaml', 'coloredlogs',
-                        'torchsummary', 'verboselogs',
-                        'future',
-                        'setuptools', 'tqdm'],
-      packages=find_packages())
+
+setup(
+    name='ConfidNet',
+    author='Charles Corbiere',
+    url='https://github.com/valeoai/ConfidNet',
+    ext_modules=[CppExtension('confignet.structured_map_ranking_loss',
+                              ['cpp_loss/structured_map_ranking_loss.cpp'])],
+    cmdclass={'build_ext': BuildExtension},
+    install_requires=['pyyaml', 'coloredlogs',
+                      'torchsummary', 'verboselogs',
+                      'future',
+                      'setuptools', 'tqdm'],
+    packages=find_packages())

1edc702181ef44eedaa48ada97756dd517701932
Charles CORBIERE <corbi_c2@auditeur.cnam.fr>
2019-09-30 10:58:04 +0200

    Add runtest2 for pathlib

---
+++ run_tests2.sh
@@ -0,0 +1,13 @@
+set -e
+
+export DOCKER_HOST=ssh://shared_account@10.3.54.102
+DOCKER_BUILDKIT=1 docker build --progress=plain -t test_image .
+
+docker run \
+          --runtime=nvidia \
+          -e NVIDIA_VISIBLE_DEVICES=0 \
+          -v svhn:/svhn-data \
+          --rm \
+          --cpus=5 \
+          -t \
+          test_image bash -c "python confidnet/test.py -c confidnet/confs/exp_mnist_integration_tests.yaml -e 56"
